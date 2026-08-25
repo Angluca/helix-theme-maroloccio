55a42eaf1cb4d804438e531f0d243434af67fdef
Pixeller <156404+Angluca@users.noreply.github.com>
2026-08-25 21:59:41 +0800

    Add mach highlight.scm

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-cp maroloccio.toml ~/.config/helix/themes/maroloccio/
+cp maroloccio.toml ~/.config/helix/themes/maroloccio/  
+`mach-lang` `highlight.scm`  
+<img width="730" alt="Jietu20260825-215127" src="https://github.com/user-attachments/assets/075cb426-42fe-4532-b0f5-bc0d42226e2a" />
---
+++ queries/mach/highlights.scm
@@ -0,0 +1,205 @@
+; Mach Tree-sitter Highlight Queries
+
+; Comments
+(comment) @comment.line
+
+; Declaration keywords
+"use" @keyword.import
+"fwd" @keyword.import
+"pub" @keyword.modifier
+"ext" @keyword.modifier
+"def" @keyword.type
+"rec" @keyword.type
+"uni" @keyword.type
+"val" @keyword.storage
+"var" @keyword.storage
+"fun" @keyword.function
+"test" @keyword.function
+
+; Control flow keywords
+"if" @keyword.conditional
+"or" @keyword.conditional
+"for" @keyword.repeat
+"ret" @keyword.return
+"brk" @keyword.control
+"cnt" @keyword.control
+"fin" @keyword.control
+
+; value after '.'
+(field_expression field: (identifier) @keyword.repeat)
+
+; Assembly
+"asm" @keyword
+
+((identifier) @type.macro
+  (#match? @type.macro "^[_]*[A-Z][A-Z0-9_]*$"))
+
+; Literals
+(integer_literal) @number
+(float_literal) @number.float
+(char_literal) @character
+(string_literal) @string
+(nil_literal) @constant.builtin
+
+; Types
+(primitive_type) @type.builtin
+
+(type_identifier) @type
+
+(record_declaration
+  name: (identifier) @type.definition)
+
+(union_declaration
+  name: (identifier) @type.definition)
+
+(type_alias_declaration
+  name: (identifier) @type.definition)
+
+(type_parameters
+  (identifier) @type.parameter)
+
+(type_arguments
+  (_) @type)
+
+(generic_type
+  name: (identifier) @type)
+
+(generic_type
+  name: (type_identifier) @type)
+
+; Functions
+(function_declaration
+  name: (identifier) @function.definition)
+
+(parameter
+  name: (identifier) @variable.parameter)
+
+(pack_parameter
+  name: (identifier) @variable.parameter)
+
+(call_expression
+  function: (identifier) @function.call)
+
+(call_expression
+  function: (field_expression
+    field: (identifier) @function.method.call))
+
+; Pack spread (va...)
+(pack_spread_expression
+  "..." @punctuation.special)
+
+; Comptime field projection (v.[f])
+(projection_expression
+  "." @punctuation.delimiter
+  "[" @punctuation.bracket
+  "]" @punctuation.bracket)
+
+; Fields and variables
+;(field_declaration
+  ;name: (identifier) @property)
+
+;(field_expression
+  ;field: (identifier) @property)
+
+(initializer_field
+  name: (identifier) @keyword.repeat)
+
+;(value_declaration
+  ;name: (identifier) @variable)
+
+;(variable_declaration
+  ;name: (identifier) @variable)
+
+; Modules
+(use_declaration
+  alias: (identifier) @variable.module)
+
+(forward_declaration
+  alias: (identifier) @variable.module)
+
+(module_path
+  (identifier) @module)
+
+; Test declarations
+(test_declaration
+  name: (string_literal) @string.special)
+
+; Compile-time
+(comptime_if_declaration
+  "$" @keyword.directive
+  "if" @keyword.directive)
+
+(comptime_or_declaration_clause
+  "$" @keyword.directive
+  "or" @keyword.directive)
+
+(comptime_if_statement
+  "$" @keyword.directive
+  "if" @keyword.directive)
+
+(comptime_or_clause
+  "$" @keyword.directive
+  "or" @keyword.directive)
+
+(comptime_each_statement
+  "$" @keyword.directive
+  "each" @keyword.directive
+  "in" @keyword.directive)
+
+(comptime_expression
+  "$" @keyword.directive)
+
+(comptime_expression
+  name: (identifier) @function.builtin)
+
+(comptime_field_path
+  (identifier) @variable.builtin)
+
+; Assembly
+(asm_statement
+  isa: (identifier) @attribute)
+
+(asm_body) @string.special
+
+; Operators
+(binary_expression
+  operator: _ @operator)
+
+(unary_expression
+  operator: _ @operator)
+
+(cast_expression
+  operator: _ @operator)
+
+(assignment_expression
+  "=" @operator)
+
+; Punctuation
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+
+";" @punctuation.delimiter
+":" @punctuation.delimiter
+"," @punctuation.delimiter
+"." @punctuation.delimiter
+
+"!" @keyword.exception
+"~" @keyword.exception
+"^" @keyword.exception
+"?" @keyword.exception
+"@" @keyword.exception
+
+"*" @operator
+"&" @operator
+
+; #[attr] decorators
+(decorator
+  "#[" @punctuation.special
+  ;name: (identifier) @attribute
+  name: (identifier) @punctuation.special
+  "]" @punctuation.special
+  ) @punctuation.special
